037897795f0c8b390ecb1147f37a000a0502ab05
magic76 <shanliyoung@gmail.com>
2022-11-23 09:43:07 +0800

    1 file modified

--- fetchWithRetry.test.ts
+++ fetchWithRetry.test.ts
@@ -1,30 +1,3 @@
-// const fetchWithRetry = async (fetchApi, baseTimeout = 1000, retryCount = 1) => {
-//   let result;
-
-//   const retryApi = async () => {
-//     if (retryCount === 0) {
-//       return {};
-//     }
-//     return new Promise((res) => {
-//       setTimeout(
-//         async () =>
-//           res(await fetchWithRetry(fetchApi, baseTimeout * 2, retryCount - 1)),
-//         baseTimeout
-//       );
-//     });
-//   };
-
-//   try {
-//     result = await fetchApi();
-//     if (!result || result.success === undefined) {
-//       throw new Error();
-//     }
-//   } catch (error) {
-//     return retryApi();
-//   }
-//   return result;
-// };
-
 import { fetchWithRetry } from './fetchWithRetry';
 
 const callApiSuccess = async () => ({
